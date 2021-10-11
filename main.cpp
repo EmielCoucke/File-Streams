ac78689eccf24ad11fe276ce1838ab3153722012
Emiel Coucke <emiel.coucke11@gmail.com>
2021-10-11 18:21:15 +0200

    sum and average

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <fstream>
 
+
 using namespace std;
 
+  
 int main() {
+
+  int sum = 0;
+  int count = 0;
 
   ifstream input;
   input.open("files/random.txt");  
@@ -12,8 +17,7 @@ int main() {
     cerr << "Could not open the file in reading mode" << endl;
   }
 
-  int sum = 0;
-  int count = 0;
+
   string line;
   while (getline(input, line)) {
     sum = sum + stoi(line);
@@ -21,19 +25,18 @@ int main() {
   }
 
   cout << "The sum is: " << sum << endl;
-  input.close();
-
+  
+  int average = sum/count;
+  cout << "The average is: " << average;
+  
+  
   ofstream output;
   output.open("files/calculations.txt");
-
-
-  int average = sum/count;
-  cout << "The average is: " << average << endl;
-
 
  
   output << sum << " " << average;
 
+  input.close();
   output.close();
 
   return 0;
